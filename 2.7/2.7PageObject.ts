1a8c0d5768c140cfe10d5432a02137483ac95b2d
Domeni <stephenxwulff@gmail.com>
2024-06-05 22:25:08 -0400

    Stephen Changes for 2.7

--- 2.7/2.7PageObject.ts
+++ 2.7/2.7PageObject.ts
@@ -2,12 +2,12 @@ import { BasePage } from "./basePage";
 import {By} from 'selenium-webdriver'; 
 
 export class pageObject extends BasePage {
-    addEm: By = By.//fill in the blank; 
-    newEm: By = By.// fill in the blank
-    namInp: By = By.// fill in the blank
-    phoInp: By = By.// fill in the blank
-    titleInp: By = By.//fill in the blank
-    saveBtn: By = By.//fill in the blank
+    addEm: By = By.name('addEmployee'); // Assuming the add button has an id 'addEmployee'
+    newEm: By = By.xpath('//li[text()="New Employee"]'); // Assuming the new employee element has an id 'New Employee'
+    namInp: By = By.xpath('(//input[@class="materialInput"])[1]');  // Assuming the name input field has a name attribute 'materialInput'
+    phoInp: By = By.name('phoneEntry'); // Assuming the phone input field has a name attribute 'phoneEntry'
+    titleInp: By = By.name('titleEntry'); // Assuming the title input field has a name attribute 'titleEntry'
+    saveBtn: By = By.id('saveBtn'); // Assuming the save button has an id 'saveBtn'
 
     constructor(){
         super({url: 'https://devmountain-qa.github.io/employee-manager/1.2_Version/index.html'});
--- 2.7/2.7Test.test.ts
+++ 2.7/2.7Test.test.ts
@@ -1,23 +1,40 @@
 import { pageObject } from "./2.7PageObject";
-const page = new pageObject(); 
+const pages = new pageObject(); 
 
 class Intern {
-   //create a class for interns that includes the name phone and title for the intern.
+    name: string; 
+    phone: number; 
+    title: string; 
+
+    constructor(name:string, phone: number, title: string){
+        this.name= name; 
+        this.phone = phone; 
+        this.title = title; 
+    }; 
 }; 
 
 let newInterns: Array<Intern> = [
-   //create an array of Interns using your class 
+  new Intern("John Doe", 1234567890, "Software Engineer"),
+  new Intern("Jane Smith", 2345678901, "Quality Assurance"),
+  new Intern("Alice Johnson", 3456789012, "Product Manager"),
+  new Intern("Paul Rudd", 8018018801, "General Manager"),
 ];
 
 let addInterns = async (newInterns) => {
-   //create a function to add the interns from your array
+    await pages.click(pages.addEm); 
+    await pages.click(pages.newEm); 
+    await pages.setInput(pages.namInp, newInterns.name); 
+    await pages.setInput(pages.phoInp, newInterns.phone); 
+    await pages.setInput(pages.titleInp, newInterns.title); 
+    await pages.click(pages.saveBtn); 
+    await pages.driver.sleep(3000); 
 };
 
 test('can add the bake off crew',async () => {
-    await two.navigate(); 
-    await two.getElement(two.addEm); 
+    await pages.navigate(); 
+    await pages.getElement(pages.addEm); 
     for(let i= 0; i < newInterns.length; i ++){
         await addInterns(newInterns[i]); 
     }; 
-    await two.driver.quit(); 
+    await pages.driver.quit(); 
 })
